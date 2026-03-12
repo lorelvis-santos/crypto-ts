3d0a7f787e7bed29063a7520af858b9ca7763325
Lorelvis Santos <santoslorelvis@gmail.com>
2026-03-12 10:20:33 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="app container">
+        <h1 className="app__title">
+          Cotizador de <span>Criptomonedas</span>
+        </h1>
+
+        <div className="app__content"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
