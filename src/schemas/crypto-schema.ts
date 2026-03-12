366b22fd9fd9f5ca033be9fd6722d1106f36beb4
Lorelvis Santos <santoslorelvis@gmail.com>
2026-03-12 11:47:20 -0400

    feat: add CryptoCurrenciesResponseSchema and implement getCryptos function for fetching cryptocurrency data

--- src/schemas/crypto-schema.ts
+++ src/schemas/crypto-schema.ts
@@ -4,3 +4,23 @@ export const CurrencySchema = z.object({
   code: z.string(),
   name: z.string(),
 });
+
+export const CryptoCurrencyResponseSchema = z
+  .object({
+    NAME: z.string(),
+    SYMBOL: z.string(),
+    LOGO_URL: z.string(),
+  })
+  .transform((val) => ({
+    name: val.NAME,
+    symbol: val.SYMBOL,
+    logoUrl: val.LOGO_URL,
+  }));
+
+export const CryptoCurrenciesResponseSchema = z
+  .object({
+    LIST: z.array(CryptoCurrencyResponseSchema),
+  })
+  .transform((val) => ({
+    list: val.LIST,
+  }));
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,21 @@
 import { create } from "zustand";
+import axios from "axios";
+import { CryptoCurrenciesResponseSchema } from "../schemas/crypto-schema";
+
+async function getCryptos() {
+  const url =
+    "https://data-api.coindesk.com/asset/v1/top/list?page=1&page_size=20&sort_by=CIRCULATING_MKT_CAP_USD&sort_direction=DESC&groups=ID,BASIC,SUPPLY,PRICE,MKT_CAP,VOLUME,CHANGE,TOPLIST_RANK&toplist_quote_asset=USD";
+
+  const {
+    data: { Data },
+  } = await axios(url);
+
+  const result = CryptoCurrenciesResponseSchema.safeParse(Data);
+  console.log(result);
+}
 
 export const useCryptoStore = create(() => ({
   fetchCryptos: () => {
-    console.log("Desde fetchCryptops");
+    getCryptos();
   },
 }));
